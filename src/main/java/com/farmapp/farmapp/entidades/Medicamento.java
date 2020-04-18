0cec496b663c17be7012295b901e34633eaa3625
luanhroliveira <luanhigor98@gmail.com>
2020-04-18 01:32:56 -0300

    H2 database, test profile, JPA

--- src/main/java/com/farmapp/farmapp/entidades/Medicamento.java
+++ src/main/java/com/farmapp/farmapp/entidades/Medicamento.java
@@ -2,11 +2,20 @@ package com.farmapp.farmapp.entidades;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Medicamento implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idMedicamento;
+	
 	private String nome;
 	private String descricao;
 	private Double preco;
